95c5af9d01d826eaf9f56f1180e7a394e7b89275
wilgen jareth vasquez torrez <est.wvasquezt0201@uml.edu.ni>
2023-02-09 08:51:33 -0600

    Actualizacion

--- src/modelos/trimestres.js
+++ src/modelos/trimestres.js
@@ -3,17 +3,18 @@ import { app } from '../js/firebase'
 
 // Importar los modulos de firebase necesarios
 import {
-    getFirestore, 
+    getFirestore,
     collection,
-    getDocs, 
+    getDocs,
     onSnapshot,
     addDoc,
     doc,
     getDoc,
-    updateDoc, 
-    QuerySnapshot} from 'firebase/firestore' 
+    updateDoc
+} from 'firebase/firestore'
 
-    //Conectarnos a la base de datos
+
+//Conectarnos a la base de datos
 export const db = getFirestore();
 
 //Guardar un trimestre
@@ -21,19 +22,24 @@ export const guardarTrimestre = (datos) => {
     addDoc(collection(db, 'trimestres'), datos)
 }
 
-//Obtener la colleccion de trimestres y mantener el estado actualizado
+//Obtener la colección de trimestres y mantenr el estado actualizado
 export const obtenerTrimestres = (callback) => {
     onSnapshot(collection(db, 'trimestres'), (querySnapshot) => {
         const trimestres = [];
         querySnapshot.forEach((doc) => {
-            trimestres.push({...doc.data(), id: doc.id})
+            trimestres.push({ ...doc.data(), id: doc.id })
         })
         callback(trimestres)
     })
-    
 }
 
-//Obtener un tremestre por su id
+//Obtener un trimestre por su id
 export const obtenerTrimestre = async (id) => {
-    const trimestreDoc = await getDoc(doc(db, 'trimestres'))
+    const trimestreDoc = await getDoc(doc(db, 'trimestres', id))
+    return { ...trimestreDoc.data(), id: trimestreDoc.id }
+}
+
+//Actualizar un trimestre por su id
+export const actualizarTrimestre = async (id, datos) => {
+    await updateDoc(doc(db, 'trimestres', id), datos)
 }
