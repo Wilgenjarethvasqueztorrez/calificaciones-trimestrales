30fa7e31ba98aab06022e49d5727a2b25f9b5b39
wilgen jareth vasquez torrez <est.wvasquezt0201@uml.edu.ni>
2025-07-16 16:33:53 -0600

    Actualizacion

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  root: "./",
-  base: "/calificaciones-trimestrales/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets"
-  },
-  plugins: []
+import { defineConfig } from "vite";  
+  
+export default defineConfig({  
+  root: "./",  
+  base: "/calificaciones-trimestrales/", 
+  build: {  
+    outDir: "dist",  
+    assetsDir: "assets",  
+    rollupOptions: {  
+      external: [],  
+      output: {  
+        manualChunks: undefined  
+      }  
+    }  
+  },  
+  plugins: []  
 });
