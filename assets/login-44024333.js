0ac5449de9599026c954baee49d1bc9d6d9e1ac6
Wilgenjarethvasqueztorrez <Wilgenjarethvasqueztorrez@users.noreply.github.com>
2025-07-17 00:33:26 +0000

    deploy: 3629cfbe002fb17bbafb3e3ac22f704d17487227

---
+++ assets/login-44024333.js
@@ -0,0 +1,1 @@
+import{l as o}from"./auth-458e6c87.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
--- assets/login-48964a89.js
+++
@@ -1,1 +0,0 @@
-import{l as o}from"./auth-6f162e42.js";const t=document.querySelector("#btn-google");t.addEventListener("click",e=>{e.preventDefault(),o()});
